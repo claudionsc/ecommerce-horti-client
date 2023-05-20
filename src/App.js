654f9f7bf461b8fc5ca84878152eb1fd415cdca2
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-05-20 14:06:13 -0300

    pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Inicial from "./components/pages/inicial/Inicial";
-import { Outlet } from "react-router-dom";
+import Cart from './components/pages/cart/Cart'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header/Header';
 import "./styles/Global.css"
 
 function App() {
- 
+
   return (
     <div className="Main">
-       <Inicial />
-       <Outlet />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element = {<Inicial />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Cart from './components/pages/cart/Cart'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './components/Header/Header';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -11,13 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route exact path='/' element={<App />} />
-        <Route path='/cart' element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </Provider>
 );
 
